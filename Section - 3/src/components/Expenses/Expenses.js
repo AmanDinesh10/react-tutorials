6008ea049a50b31e3928e0da19e16c6a40cf1ab8
Aman Dinesh <xarmaaaruti@gmail.com>
2022-06-20 12:45:15 +0530

    Section 5 Completed

--- Section - 3/src/components/Expenses/Expenses.js
+++ Section - 3/src/components/Expenses/Expenses.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -11,21 +12,17 @@ const Expenses = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
+
+    const filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {
-                    props.expenses.map((expense) => {
-                        return (
-                            <ExpenseItem key={expense.id}
-                                title={expense.title}
-                                amount={expense.amount}
-                                date={expense.date}
-                            />
-                        )
-                    })
-                }
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
